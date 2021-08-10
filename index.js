1974e0c30729a4f44e99766fe7643619b3d32df4
reb0und <67512765+reb0und@users.noreply.github.com>
2021-08-10 14:41:39 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 var axios = require('axios');
 
 var args = process.argv[2];
-console.log(args);
 
+args.replace(/\\/, '%');
+args.replace(/, /, '+');
+args.replace(/"/, '');
+args.replace(/'/, '');
 
 var data = `tool=ascii-code&ciphertext=${args}&output_hexa=false`;
 
@@ -13,19 +16,25 @@ var config = {
     'x-requested-with': 'XMLHttpRequest',
     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36',
     'referer': 'https://www.dcode.fr/ascii-code',
-    'cookie': 'PHPSESSID=3bea32d8d3fb5a571126a89c99bd80e9;'
+    'cookie': 'PHPSESSID=6965dd8ae33c386093dba0f3f3328040;'
   },
   data: data
 };
+
+var idx = 0;
 
 axios(config)
   .then(function (response) {
     if (response.data.results == undefined)
       console.log('Error parsing data');
     else
-      console.log(JSON.stringify(response.data.results));
+      var key = Object.keys(response.data.results)[idx];
+    value = response.data.results[key]
+    console.log(value);
+
   })
   .catch(function (error) {
     console.log(error);
   });
+
 
